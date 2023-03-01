6d469144043ed1d57d6425ad5897046fa7865e00
Adelina <06AdaigithubAraratyan!>
2023-03-01 14:35:32 +0400

    addMobileAdapts

--- SRC/js/script.js
+++ SRC/js/script.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
         },{
             breakpoint:575,
             settings:{
-                slidesToShow:2
+                slidesToShow:1
             }
         }],
         mobileFirst:false,
@@ -105,6 +105,87 @@ $(document).ready(function(){
         speed:1000,
         asNavFor:".about__smslider"
         
+    });
+
+    $('.about__mbslder').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:false,
+        autoplaySpeed:1500,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        draggable:true,
+        swipe:true,
+        touchThreshold:25,
+        touchMove:true,
+        waitForAnimate:true,
+        centerMode:false,
+        variableWidth:false,
+        rows:1,
+        slidesPerRow:1,
+        vertical:false,
+        verticalSwiping:false,
+        
+        asNavFor:".orders__bslider",
+        responsive:[{
+            breakpoint:991,
+            settings:{
+                slidesToShow:3,
+                slidesToScroll:1,
+                infinite: true,
+                dots: true
+            }
+        },{
+            breakpoint:575,
+            settings:{
+                slidesToShow:1
+            }
+        }],
+        mobileFirst:false,
+        appendArrows:$('.content'),
+        /* appendDots:$('.content') */
+    });
+
+    $('.orders__mbslider').slick({
+        arrows:false,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        easing:'ease',
+        infinite:true,
+        initialSlide:0,
+        autoplay:false,
+        autoplaySpeed:1500,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        draggable:true,
+        swipe:true,
+        touchThreshold:25,
+        touchMove:true,
+        waitForAnimate:true,
+        centerMode:false,
+        variableWidth:false,
+        rows:1,
+        slidesPerRow:1,
+        vertical:false,
+        verticalSwiping:false,
+        
+        asNavFor:".orders__bslider",
+       
+        mobileFirst:false,
+        appendArrows:$('.content'),
+        /* appendDots:$('.content') */
     });
 
 });
